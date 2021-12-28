9127a5f80fa9c357fd3b72f7acb766301160d4aa
Alibek Kaliyev <alk224@lehigh.edu>
2021-12-28 18:49:27 +0600

    Updates

---
+++ component---src-pages-404-js-d05dda738a25b84f28bd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("wiF5"),o=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d05dda738a25b84f28bd.js.map
